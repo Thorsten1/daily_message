b698a417ad8ce417614da4d3e5efa512bef53e8f
Thorsten Rösler <thorsten.roesler@alumni.fh-aachen.de>
2021-04-20 19:08:08 +0200

    Add first Methods - add get, set, delete and random get endpoint

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-daily_message
+# daily_message
+This project provides a small Express based REST API to run on [repl.it](https://replit.com).
+
+## Local testing
+To test or debug this project locally you need to provide the environment variable <code>REPLIT_DB_URL</code>
+
+### ToDos
+- Add authorization Tokens for destructive and maybe also non-destructive operations
+- Add Daily check so only one message will be prompted per day (always the same)
+- Maybe add edit endpoint
+- Make use of morgan's extended logging
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');  // Node.js body parsing middleware.
 const cors = require('cors'); // Set CORS Headers
 const helmet = require('helmet'); // Set secure HTTP Headers
 const morgan = require('morgan'); // Extended Logging
+const Client = require("@replit/database"); //repl.it key/value database
+
 
 
 // Start of the actual App
@@ -15,14 +17,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet())
 app.use(cors());
 app.use(morgan('combined'));
+const client = new Client(); //initialise repl.it dv client
 
-app.get('/', (req, res) => {
-    return res.send('This seems to work.')
+
+// get endpoint to receive random message
+// TODO: implement daily check so only one message per day
+app.get('/', async (req, res) => {
+    let keys = await client.list()
+    // get random element
+    result_key = keys[Math.floor(Math.random() * keys.length)]
+    let result = await client.get(result_key, {raw:true})
+    return res.send(result)
 });
 
-// start the server on Port 3000
-app.listen(3000, () => {
-    console.log(`Example app listening on port 3000!`);
+// get endpoint to retrieve specific messages via its key
+app.get('/get/:key', async (req, res) => {
+    // raw value to simply store and get string values
+    let result = await client.get(req.params.key, {raw:true})
+    return res.send(result)
+});
 
+// delete endpoint to delete specific message via its key
+app.delete('/get/:key', async (req, res) => {
+    // delete the provided key
+    await client.delete(req.params.key)
+    // Always return 200 because repl.it db gives no feedback whether the key existed or the call wa successful
+    return res.status(200)
+});
+
+//post endpoint to upload new endpoints
+app.post('/add', async (req, res) => {
+    // This will overwrite keys
+    // Maybe add edit method and prevent this in the future
+    await client.set(req.body.key, req.body.value);
+    // Always return 200 because repl.it db gives no feedback whether the key existed or the call wa successful
+    return res.status(200)
+});
+
+
+const port = 3000 // start the server on this port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}!`);
 });
 
